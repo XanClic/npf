d2a9ea7b0e3da6d743ad8eb172b7e54d5a8b67de
Hanna Reitz <xanclic@xanclic.moe>
2018-06-06 01:03:44 +0200

    Ich hab hier diese lokale Änderung
    …und dachte, ich committe die einfach mal, egal lol euer clici

--- npf2bmp.c
+++ npf2bmp.c
@@ -138,9 +138,9 @@ int main(int argc, char *argv[])
     struct npfc_list *cle = cl;
     while (cle != NULL)
     {
-        if ((int)(cle->chr->num >> 5) != cline)
+        if ((int)(cle->chr->num >> 4) != cline)
         {
-            cline = cle->chr->num >> 5;
+            cline = cle->chr->num >> 4;
             lines++;
         }
 
@@ -150,7 +150,7 @@ int main(int argc, char *argv[])
 
     struct bmp_header bmph = {
         .type = "BM",
-        .sz = (lines * (fh + 1) - 1) * (32 * (fw + 1) - 1) * 3 + sizeof(struct bmp_header) + sizeof(struct bmp_info),
+        .sz = (lines * (fh + 1) - 1) * (16 * (fw + 1) - 1) * 3 + sizeof(struct bmp_header) + sizeof(struct bmp_info),
         .offset = sizeof(struct bmp_header) + sizeof(struct bmp_info)
     };
 
@@ -158,7 +158,7 @@ int main(int argc, char *argv[])
 
     struct bmp_info bmpi = {
         .size = sizeof(bmpi),
-        .width = 32 * (fw + 1) - 1,
+        .width = 16 * (fw + 1) - 1,
         .height = -(lines * (fh + 1) - 1),
         .planes = 1,
         .bpp = 24,
@@ -172,20 +172,20 @@ int main(int argc, char *argv[])
 
     fwrite(&bmpi, sizeof(bmpi), 1, bmp);
 
-    size_t line_length = (32 * (fw + 1) * 3) & ~3;
+    size_t line_length = (16 * (fw + 1) * 3) & ~3;
     size_t bufsz = line_length * (fh + 1);
     uint8_t *buf = malloc(bufsz);
 
     cle = cl;
     while (cle != NULL)
     {
-        unsigned cline = cle->chr->num >> 5;
+        unsigned cline = cle->chr->num >> 4;
 
         memset(buf, 0xFF, bufsz);
 
-        while ((cle != NULL) && ((cle->chr->num >> 5) == cline))
+        while ((cle != NULL) && ((cle->chr->num >> 4) == cline))
         {
-            uint8_t *pos = buf + (cle->chr->num & 0x1F) * (fw + 1) * 3;
+            uint8_t *pos = buf + (cle->chr->num & 0xF) * (fw + 1) * 3;
 
             for (unsigned ry = 0; ry < fh; ry++)
             {
